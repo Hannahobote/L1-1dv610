542e892e38ed8b37283165086ffebea3e9ba361e
Hannah Obote <ao223ir@student.lnu.se>
2022-09-14 16:48:28 +0200

    login complete

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,6 +1,31 @@
-class auth {
-  constructor() {
+import { userDb } from '../user-mock-db.js'
+import bcrypt from 'bcrypt'
+import { runInNewContext } from 'vm'
+
+export class Auth {
+
+  async login(req, res, next) {
+    const user = userDb.find(username => username.username === req.body.username)
+
+    console.log(user, '..', req.body.username)
+
+    if (user) {
+      // compare password with bcrypt 
+      if (bcrypt.compare(req.body.password, user.password)) {
+        res.status(200).send('user logged in')
+        // set session cookies 
+        
+      } else {
+        res.status(404).send('user not found')
+      }
+    } else {
+      return res.status(400).send('User not found')
+    }
 
   }
-  
+
+  async logout(req, res, next) {
+
+  }
+
 }
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import { User } from '../controller/user-controller.js'
+import { Auth } from '../controller/auth-controller.js'
 const user = new User()
+const auth = new Auth()
 export const router = express.Router()
 
 
 // get all user 
-router.get('/user', (req, res, next) => user.getUserDB(req, res, next))
+router.get('/user', (req, res, next) => user.getUsers(req, res, next))
 
 // create user 
 router.post('/user', (req, res, next) => user.registerUser(req, res, next))
+
+
+// login 
+router.post('/login',(req, res, next) => auth.login(req, res, next))
