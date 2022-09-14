64606a2c3de43ef89794ee853acd795ef4a91db2
Hannah Obote <ao223ir@student.lnu.se>
2022-09-14 16:20:07 +0200

    register and get users

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,28 +1,29 @@
-import { equal } from 'assert'
 import bcrypt from 'bcrypt'
+import { userDb } from '../user-mock-db.js'
 
 export class User {
 
-  constructor() {
-    this.userDb = []
-  }
 
-  getUserDB (req, res, next) {
-    res.json(this.userDb)
+  // the other crud operations?
+
+  getUsers (req, res, next) {
+    res.send(userDb)
   }
 
 
 async registerUser (req, res, next) {
     // will push an object that contains username and password 
+    
     // if there is no req body, thow error
-    if(!req.body) {
-      res.send('body requried')
+    if(!req.body.username && !req.body.password) {
+      res.status(412).send({error: 'username and password requierd in the body'})
     }
+    
     const username = req.body.username
     const password = req.body.password
     const hashPassword = await bcrypt.hash(password, 10)
-    console.log({username, password: hashPassword})
-    this.userDb.push({username, password: hashPassword})
-    res.status(201)
+
+    userDb.push({username, password: hashPassword})
+    res.sendStatus(201)
   }
 }
