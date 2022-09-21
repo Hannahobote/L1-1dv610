288b7b9f51716e1219486ac5539dc8c4b9477533
Hannah Obote <ao223ir@student.lnu.se>
2022-09-21 12:06:51 +0200

    session cookies

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,28 +1,37 @@
 import { userDb } from '../user-mock-db.js'
 import bcrypt from 'bcrypt'
-import { runInNewContext } from 'vm'
+import session from 'express-session'
 
 export class Auth {
 
   async login(req, res, next) {
     const user = userDb.find(username => username.username === req.body.username)
 
-    console.log(user, '..', req.body.username)
-
     if (user) {
-      // compare password with bcrypt 
-      if (bcrypt.compare(req.body.password, user.password)) {
-        res.status(200).send('user logged in')
-        // set session cookies 
+
+      // compare password with bcrypt. if password incorrect, throw error
+      if (await bcrypt.compare(req.body.password, user.password)) {
+
+        // set session cookies . If user is already logged in, send messege back. orelse set the session and send cookie back
+       // res.set('Set-Cookie', `session=${user.id}`)
+       req.session.regenerate(() => {
+         req.session.authenticated = true
+         req.session.username = user.username
+         req.session.userId = user.id
+        })
         
+        console.log(req.session)
+       res.status(200).send('user logged in. Check headers for session cookies.')
+
       } else {
-        res.status(404).send('user not found')
+        res.status(401).send('wrong credentials') // 403?
       }
     } else {
-      return res.status(400).send('User not found')
+      res.status(400).send('User not found')
     }
 
   }
+
 
   async logout(req, res, next) {
 
--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import { userDb } from '../user-mock-db.js'
+import { v4 as uuidv4 } from 'uuid';
 
 export class User {
 
@@ -14,7 +15,7 @@ export class User {
 async registerUser (req, res, next) {
     // will push an object that contains username and password 
     
-    // if there is no req body, thow error
+    // if there is no req body, throw error
     if(!req.body.username && !req.body.password) {
       res.status(412).send({error: 'username and password requierd in the body'})
     }
@@ -23,7 +24,8 @@ async registerUser (req, res, next) {
     const password = req.body.password
     const hashPassword = await bcrypt.hash(password, 10)
 
-    userDb.push({username, password: hashPassword})
+    userDb.push({ id: uuidv4() ,username, password: hashPassword})
     res.sendStatus(201)
+    
   }
 }
